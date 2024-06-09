95f1555b698b3219df96a9e51df76b0587b77f05
Kerwyn Rojas <kerwyn.rg@gmail.com>
2024-06-09 17:40:17 -0600

    Testing client request

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+// /* Server ENVS */
+// export const AWS_REGION: string = process.env.ENV_AWS_REGION!;
+// export const AWS_ACCESS_KEY: string = process.env.ENV_AWS_ACCESS_KEY!;
+// export const AWS_SECRET_ACCESS_KEY: string = process.env.ENV_AWS_SECRET_ACCESS_KEY!;
+// export const AWS_AGENT_ID: string = process.env.ENV_AWS_AGENT_ID!;
+// export const AWS_AGENT_ALIAS_ID: string = process.env.ENV_AWS_AGENT_ALIAS_ID!;
+
+/* Client ENVS */
+export const AWS_REGION: string = process.env.NEXT_PUBLIC_ENV_AWS_REGION!;
+export const AWS_ACCESS_KEY: string = process.env.NEXT_PUBLIC_ENV_AWS_ACCESS_KEY!;
+export const AWS_SECRET_ACCESS_KEY: string = process.env.NEXT_PUBLIC_ENV_AWS_SECRET_ACCESS_KEY!;
+export const AWS_AGENT_ID: string = process.env.NEXT_PUBLIC_ENV_AWS_AGENT_ID!;
+export const AWS_AGENT_ALIAS_ID: string = process.env.NEXT_PUBLIC_ENV_AWS_AGENT_ALIAS_ID!;
